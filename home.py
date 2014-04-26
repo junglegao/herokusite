426369f56a04223b500842fc32126cfe9bbdb44f
gaojian <junglegao@gmail.com>
2014-04-26 21:56:44 +0800

    add xiaomo

--- home.py
+++ home.py
@@ -9,4 +9,4 @@ def home():
 
 @app.route('/xiaomo')
 def xiaomo():
-  return '''Having you being my girl friend is the best honor and greatest luck\nWish you happy every day -- fighting for the future"
+  return '''Having you being my girl friend is the best honor and greatest luck\nWish you happy every day -- fighting for the future'''
