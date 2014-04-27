ce88118ff91a36e71a8e696f820e5c4734855f27
gaojian <junglegao@gmail.com>
2014-04-27 13:57:20 +0800

    add time count up

--- home.py
+++ home.py
@@ -1,13 +1,20 @@
 import os
 from flask import Flask
 from flask import render_template
+import datetime
 
 app = Flask(__name__)
-
+app.config['DEBUG'] = True
 @app.route('/')
 def home():
   return "Welcome to Jian's world!"
 
 @app.route('/xiaomo')
 def xiaomo():
-  return render_template("home.html")
+  delta_t = datetime.datetime.now()-datetime.datetime(2014, 2, 23, 21, 31, 48)
+  day = delta_t.days
+  hour = delta_t.seconds/3600
+  minute = delta_t.seconds%3600/60
+  second = delta_t.seconds%3600%60
+  t_till_now = {'day':day, 'hour': hour, 'minute':minute, 'second':second}
+  return render_template("home.html",t=t_till_now)
