10905d92758a45d6c306c2c1f2ef963307f0e184
gaojian <junglegao@gmail.com>
2014-04-28 14:12:41 +0800

    add time count up using js

--- home.py
+++ home.py
@@ -12,11 +12,4 @@ def home():
 
 @app.route('/xiaomo')
 def xiaomo():
-  delta_t = datetime.datetime.now(pytz.timezone('Asia/Hong_Kong'))-\
-      datetime.datetime(2014, 2, 23, 21, 31, 48, tzinfo=pytz.timezone('Asia/Hong_Kong'))
-  day = delta_t.days
-  hour = delta_t.seconds/3600
-  minute = delta_t.seconds%3600/60
-  second = delta_t.seconds%3600%60
-  t_till_now = {'day':day, 'hour': hour, 'minute':minute, 'second':second}
-  return render_template("home.html",t=t_till_now)
+  return render_template("home.html")
